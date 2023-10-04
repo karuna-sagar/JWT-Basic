b4702ea7e4c82f73d531f28492841bda09951df7
Karuna sagar <karunasagar129130@gmail.com>
2023-10-04 16:59:43 +0530

    second commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,13 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = (req, res) => {
     res.send('Fake Login/Rsgister')
 }
 const dashboard = (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and password', 404)
+    }
     const luckyNumber = Math.floor(Math.random() * 100);
     res.status(200).json({ msg: `hello joe`, secret: `Here is yor data authorised number lucky number is ${luckyNumber}` })
 }
